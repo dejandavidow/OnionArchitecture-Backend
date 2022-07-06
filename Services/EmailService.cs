a6a3809f5ccb8dc8ff863d87acd0cd7c85270560
dejan <dejandavidovict@gmail.com>
2022-07-06 18:25:18 +0200

    ads

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -29,7 +29,7 @@ namespace Services
             builder.HtmlBody = mailRequest.Body;
             email.Body = builder.ToMessageBody();
             using var smtp = new SmtpClient();
-            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Connect(_mailSettings.Host,_mailSettings.Port,true);
             smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
             await smtp.SendAsync(email);
             smtp.Disconnect(true);
