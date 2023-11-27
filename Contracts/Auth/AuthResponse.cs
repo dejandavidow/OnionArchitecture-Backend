ba04cef13490ca12945c7e0db6d4da618663690e
Dejan Davidović <dejandavidovict@gmail.com>
2023-11-27 17:24:55 +0100

    contracts

--- Contracts/Auth/AuthResponse.cs
+++ Contracts/Auth/AuthResponse.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Contracts.Auth
+﻿namespace Contracts.Auth
 {
     public class AuthResponse
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Role { get; set; }
-        public string AccessToken { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }
     }
 }
